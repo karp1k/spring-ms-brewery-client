41a8f9df6a6a63b24fd150ed21a86a6caa8fd24f
Tom <desktop-vm-ubuntu>
2020-04-15 18:54:17 +0300

    Externalized properties for httpclient

--- src/main/java/guru/springframework/springmsbreweryclient/web/config/BlockingRestTemplateCustomizer.java
+++ src/main/java/guru/springframework/springmsbreweryclient/web/config/BlockingRestTemplateCustomizer.java
@@ -1,11 +1,13 @@
 package guru.springframework.springmsbreweryclient.web.config;
 
+import lombok.Setter;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateCustomizer;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -17,17 +19,24 @@ import org.springframework.web.client.RestTemplate;
  * @author kas
  */
 @Component
+@ConfigurationProperties(value = "resttemplate") // Spring uses setter injection (required setters for work)
+@Setter
 public class BlockingRestTemplateCustomizer implements RestTemplateCustomizer {
+
+    private Integer maxTotalConnections;
+    private Integer defaultMaxPerRoute;
+    private Integer connectionRequestTimeout;
+    private Integer socketTimeout;
 
     ClientHttpRequestFactory apacheClientHttpRequestFactory() {
         PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
-        connectionManager.setMaxTotal(100);
-        connectionManager.setDefaultMaxPerRoute(20);
+        connectionManager.setMaxTotal(maxTotalConnections);
+        connectionManager.setDefaultMaxPerRoute(defaultMaxPerRoute);
 
         RequestConfig requestConfig = RequestConfig
                 .custom()
-                .setConnectionRequestTimeout(3000) // if request proceed more than 3000 then Fail
-                .setSocketTimeout(3000)
+                .setConnectionRequestTimeout(connectionRequestTimeout) // if request proceed more than 3000 then Fail
+                .setSocketTimeout(socketTimeout)
                 .build();
 
         CloseableHttpClient client = HttpClients
